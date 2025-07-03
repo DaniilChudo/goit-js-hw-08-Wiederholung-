a96c1a1f20e5120352166f9e97a7254aa70d8a7e
DaniilChudo <daniilpro1994@gmail.com>
2025-07-03 14:03:24 +0200

    Fertig

--- js/gallery.js
+++ js/gallery.js
@@ -66,6 +66,22 @@ const images = [
 
 const gallery = document.querySelector(".gallery");
 
+gallery.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const isImageEl = event.target.classList.contains("gallery-image");
+  if (!isImageEl) return;
+
+  const largeImageURL = event.target.dataset.source;
+  const description = event.target.alt;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" alt="${description}" />
+  `);
+
+  instance.show();
+});
+
 const newGallery = images
   .map(
     (foto) => `<li class="gallery-item">
